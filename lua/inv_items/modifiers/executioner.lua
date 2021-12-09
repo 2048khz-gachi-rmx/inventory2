2b5cb2f0a61be3d6a7b5761312a086de43b6b917
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2021-12-09 20:22:24 +0700

    Executioner mod

---
+++ lua/inv_items/modifiers/executioner.lua
@@ -0,0 +1,69 @@
+setfenv(0, _G)
+local numCol, notNumCol, textCol = unpack(Inventory.Modifiers.DescColors)
+
+local dmgMult = 1.35
+
+local el; el = Inventory.BaseModifier:new("Executioner")
+	:SetMaxTier(3)
+	:SetMinBPTier(2)
+	:SetMaxBPTier(4)
+	:Hook("EntityTakeDamage", function(self, ent, dmg)
+		if not ent:IsPlayer() then return end
+
+		local hp, ar = ent:Health(), ent:Armor()
+		local total = hp + ar
+
+		local str = self:GetTierStrength(self:GetTier())
+		if not str then return end
+
+		if total > str then return end
+
+		dmg:ScaleDamage(dmgMult)
+	end)
+	:SetTierCalc(function(self, tier)
+		-- 50 / 75 / 100
+		return 25 + tier * 25
+	end)
+
+--[=[
+local recipes = {
+	{"thruster_t1", 2},
+	{"thruster_t2", 2},
+	{"thruster_t2", 4},
+}
+
+el  :On("AlterRecipe", "a", function(self, itm, rec, tier)
+		rec[recipes[tier][1]] = recipes[tier][2]
+	end)
+]=]
+
+function el:GenerateMarkup(it, mup, tier)
+	local mod = mup:AddPiece()
+	mod:SetAlignment(1)
+	mod.Font = "WEIRD22"
+
+	local tx = mod:AddText("Executioner " .. string.ToRoman(tier))
+	mod:SetColor(Color(220, 60, 60))
+
+	local desc = mup:AddPiece()
+	desc.Font = "OS16"
+	desc:DockMargin(8, 0, 8, 0)
+	desc:SetColor(textCol)
+	desc:SetAlignment(1)
+
+	desc:AddText("Deal ")
+	desc:AddText(dmgMult * 100 - 100 .. "% ").color = numCol
+	desc:AddText("more damage to players below ")
+
+	for i=1, self:GetMaxTier() do
+		local tx2 = desc:AddText(tostring(self:GetTierStrength(i)))
+		tx2.color = i == tier and numCol or notNumCol
+
+		if i ~= self:GetMaxTier() then
+			local sep = desc:AddText("/")
+			sep.color = notNumCol
+		end
+	end
+
+	desc:AddText(" combined health.")
+end
