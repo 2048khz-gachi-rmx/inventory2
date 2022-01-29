e2eaab0cfcdcfdb75d08e4b24978ac09527fbdeb
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-01-30 00:34:04 +0700

    update Inventory.Draw.DrawItemAmount

--- lua/inventory/client/cl_draw.lua
+++ lua/inventory/client/cl_draw.lua
@@ -3,15 +3,29 @@ Inventory.Draw = Inventory.Draw or {}
 
 local tcol = Color(0, 0, 0)
 
-function Inventory.Draw.DrawItemAmount(id, amt, font, x, y, clr, ax, ay, col2)
+function Inventory.Draw.DrawItemAmount(id, amt, font, x, y, ax, ay, clr, col2)
 	local base = Inventory.Util.GetBase(id)
 	local txt1 = "%s"
+
+	local a = 255
+	if isnumber(clr) then
+		a = clr
+		clr = nil
+	elseif IsColor(clr) then
+		a = clr.a
+	end
 
 	if not base then
 		txt = "no base (%s) "
 		txt = txt:format(id)
+		
+		tcol:Set(clr or color_white)
+		tcol.a = a
 	else
 		txt = ("%s: "):format(base:GetName())
+
+		tcol:Set(clr or base:GetColor())
+		tcol.a = a
 	end
 
 	local tx2 = ("x%s"):format(amt or "what")
@@ -21,12 +35,12 @@ function Inventory.Draw.DrawItemAmount(id, amt, font, x, y, clr, ax, ay, col2)
 	surface.SetFont(font)
 	local tw, th = surface.GetTextSize(txt1 .. tx2)
 	surface.SetTextPos(x - axm * tw, y - aym * tw)
-	surface.SetTextColor(clr:Unpack())
+	surface.SetTextColor(tcol:Unpack())
 
 	surface.DrawText(txt)
 
 	tcol:Set( col2 or color_white )
-	if not col2 then tcol.a = clr.a end
+	if not col2 then tcol.a = a end
 
 	surface.SetTextColor( tcol:Unpack() )
 	surface.DrawText(tx2)
