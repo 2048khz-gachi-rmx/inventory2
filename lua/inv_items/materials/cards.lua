731908bce69edee392d1c9cf61edd7a98d6c96e8
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-05-09 03:12:54 +0700

    cards are assigned access levels

--- lua/inv_items/materials/cards.lua
+++ lua/inv_items/materials/cards.lua
@@ -1,41 +1,59 @@
-Inventory.BaseItemObjects.Generic("card1")
-	:SetName("Level 1 Access Keycard")
-	:SetModel("models/labskeycards/normalkeycard.mdl")
+local cards = {}
 
-	:SetCamPos( Vector(54.7, -0.7, 67.1) )
-	:SetLookAng( Angle(50.8, -180.7, 0.0) )
-	:SetFOV( 3.5 )
+table.insert(cards,
+	Inventory.BaseItemObjects.Generic("card1")
+		:SetName("Level 1 Access Keycard")
+		:SetModel("models/labskeycards/normalkeycard.mdl")
 
-	:SetCountable(true)
-	:SetMaxStack(3)
-	:SetBaseTransferCost(150000)
+		:SetCamPos( Vector(54.7, -0.7, 67.1) )
+		:SetLookAng( Angle(50.8, -180.7, 0.0) )
+		:SetFOV( 3.5 )
 
-	:SetRarity("common")
+		:SetCountable(true)
+		:SetMaxStack(3)
+		:SetBaseTransferCost(150000)
 
-Inventory.BaseItemObjects.Generic("card2")
-	:SetName("Level 2 Access Keycard")
-	:SetModel("models/labskeycards/greenkeycard.mdl")
+		:SetRarity("common")
+)
 
-	:SetCamPos( Vector(54.7, -0.7, 67.1) )
-	:SetLookAng( Angle(50.8, -180.7, 0.0) )
-	:SetFOV( 3.5 )
+table.insert(cards,
+	Inventory.BaseItemObjects.Generic("card2")
+		:SetName("Level 2 Access Keycard")
+		:SetModel("models/labskeycards/greenkeycard.mdl")
 
-	:SetCountable(true)
-	:SetMaxStack(3)
-	:SetBaseTransferCost(150000)
+		:SetCamPos( Vector(54.7, -0.7, 67.1) )
+		:SetLookAng( Angle(50.8, -180.7, 0.0) )
+		:SetFOV( 3.5 )
 
-	:SetRarity("uncommon")
+		:SetCountable(true)
+		:SetMaxStack(3)
+		:SetBaseTransferCost(150000)
 
-Inventory.BaseItemObjects.Generic("card3")
-	:SetName("Level 3 Access Keycard")
-	:SetModel("models/labskeycards/bluekeycard.mdl")
+		:SetRarity("uncommon")
+)
 
-	:SetCamPos( Vector(54.7, -0.7, 67.1) )
-	:SetLookAng( Angle(50.8, -180.7, 0.0) )
-	:SetFOV( 3.5 )
+table.insert(cards,
+	Inventory.BaseItemObjects.Generic("card3")
+		:SetName("Level 3 Access Keycard")
+		:SetModel("models/labskeycards/bluekeycard.mdl")
 
-	:SetCountable(true)
-	:SetMaxStack(3)
-	:SetBaseTransferCost(150000)
+		:SetCamPos( Vector(54.7, -0.7, 67.1) )
+		:SetLookAng( Angle(50.8, -180.7, 0.0) )
+		:SetFOV( 3.5 )
 
-	:SetRarity("rare")
+		:SetCountable(true)
+		:SetMaxStack(3)
+		:SetBaseTransferCost(150000)
+
+		:SetRarity("rare")
+)
+
+for k,v in pairs(cards) do
+	v.IsKeyCard = true
+	local lvl = v:GetItemName() and tonumber(v:GetItemName():match("%d+$"))
+	v.AccessLevel = lvl or k
+
+	if not lvl then
+		errorNHf("failed to resolve card access level: %s (using: %d)", v:GetItemName(), v.AccessLevel)
+	end
+end
